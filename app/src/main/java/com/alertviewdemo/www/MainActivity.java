0dd1150940f7cea27fb0b6497b273183450ec2ed
YangLiXiang <yanglixiang@zhaoonline.com>
2016-11-03 15:55:25 +0800

    优化alertDialog弹框

--- app/src/main/java/com/alertviewdemo/www/MainActivity.java
+++ app/src/main/java/com/alertviewdemo/www/MainActivity.java
@@ -31,8 +31,10 @@ public class MainActivity extends Activity {
         mOkOrCancle01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final MyDialogView myDialog=new MyDialogView(MainActivity.this,5);
-                myDialog.setCancelable(false);
+                final MyDialogView myDialog=new MyDialogView(MainActivity.this);
+                myDialog.isShowTitle(true);
+                myDialog.isNoShowCancle(true);
+                myDialog.isNoBack(true);
                 myDialog.setMessageCenter(Html.fromHtml("<font color='#666666'>恭喜您，注册成为蓝吉星讯用户，<br/>默认初始密码为123456，</font><font color='#AA5066'>" +
                         "<br/>为了您的账户安全，请及时修改密码。<br/>" +
                         "</font><font color='#666666'>确认修改密码，请点击确定</font>"));
--- app/src/main/java/com/alertviewdemo/www/myview/MyDialogView.java
+++ app/src/main/java/com/alertviewdemo/www/myview/MyDialogView.java
@@ -10,48 +10,163 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.alertviewdemo.www.R;
-
+/**
+ * ============================================================
+ *
+ * 作 者 : 杨理想
+ *
+ * 创建日期 ：2016/11/3 15:33
+ * 
+ * 描 述 ：模Ios ,自定义弹框
+ * 
+ * 修订历史 ：
+ * 
+ * ============================================================
+ **/
 public class MyDialogView extends Dialog {
 
-    private TextView mUPwdOk,mUPwdCancel,mShowCenter;
+    private TextView mUPwdOk,mUPwdCancel,mTitle,mShowCenter; //确定，取消，标题title，消息
+    private LinearLayout mTitleLayout; //title布局,是否显示
     private int recLen = 0;
+    private View mView,mOkOrCancelLine;
+    private Context mContext;
 
-    public MyDialogView(Context context, int mTime) {
+    public MyDialogView(Context context) {
         super(context, R.style.mydialog);
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.msg_ok_cancel, null);  //通过LayoutInflater获取布局
-        mShowCenter = (TextView) view.findViewById(R.id.mShowCenter);
-        mUPwdOk = (TextView) view.findViewById(R.id.mOk);
-        mUPwdCancel = (TextView) view.findViewById(R.id.mCancel);
-        mUPwdOk.setEnabled(false);
-        mUPwdCancel.setEnabled(false);
-        setContentView(view);
+        this.mContext = context;
+        this.mView = LayoutInflater.from(getContext()).inflate(R.layout.msg_ok_cancel, null);  //通过LayoutInflater获取布局
+        //初始化view控件
+        initUi();
+        setContentView(mView);
+        //设置弹框宽度
+        initAlertDialogWidth();
+    }
 
+    /**
+     * 初始化view控件
+     */
+    private void initUi(){
+        mShowCenter = (TextView) mView.findViewById(R.id.mShowCenter);
+        mTitleLayout = (LinearLayout) mView.findViewById(R.id.mTitleLayout);
+        mOkOrCancelLine = mView.findViewById(R.id.mOkOrCancelLine);
+        mTitle = (TextView) mView.findViewById(R.id.mTitle);
+        mUPwdOk = (TextView) mView.findViewById(R.id.mOk);
+        mUPwdCancel = (TextView) mView.findViewById(R.id.mCancel);
+    }
+
+    /**
+     * 是否显示标题title
+     * @param isShow
+     */
+    public void isShowTitle(boolean isShow){
+        if(isShow){
+            mTitleLayout.setVisibility(View.VISIBLE);
+        } else {
+            mTitleLayout.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 是否显示取消
+     */
+    public void isNoShowCancle(boolean isShow) {
+        if (!isShow) {
+            mOkOrCancelLine.setVisibility(View.GONE);
+            mUPwdCancel.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * 设置是否能back
+     * @param isNoBack
+     */
+    public void isNoBack(boolean isNoBack){
+        setCancelable(!isNoBack);
+    }
+
+    /**
+     * 设置弹框宽度或高度
+     */
+    private void initAlertDialogWidth() {
+        //获取屏幕的分辨率（宽，高）
         DisplayMetrics dm = new DisplayMetrics();
-        dm = context.getApplicationContext().getResources().getDisplayMetrics();
-
+        dm = mContext.getApplicationContext().getResources().getDisplayMetrics();
+        //设置弹框的宽度和高度
         Window dialogWindow = getWindow();
         WindowManager.LayoutParams layoutParams = dialogWindow.getAttributes();
-        layoutParams.width = (int) (dm.widthPixels * 0.75);
+        layoutParams.width = (int) (dm.widthPixels * 0.75); //此处仅仅只设置弹框的宽度,也建议如此
         dialogWindow.setGravity(Gravity.CENTER_HORIZONTAL);
         dialogWindow.setAttributes(layoutParams);
+    }
+
+    /**
+     * 含有倒计时的确定或取消按钮
+     */
+    public void setCountDown (int mTime) {
         this.recLen = mTime;
-        Message message = handler.obtainMessage(1);     // Message
-        handler.sendMessageDelayed(message, 1000);
+        if(mTime > 0){
+            mUPwdOk.setEnabled(false);
+            mUPwdCancel.setEnabled(false);
+            Message message = handler.obtainMessage(1);     // Message
+            handler.sendMessageDelayed(message, 1000);
+        }
+    }
+
+    /**
+     * 设置标题内容
+     * @param title 标题内容
+     */
+    public void setTitle(CharSequence title){
+        mTitle.setText(title);
+    }
+
+    /**
+     * 设置标题大小
+     * @param mSize 标题大小
+     */
+    public void setTetileSize(float mSize){
+        mTitle.setTextSize(mSize);
+    }
+
+    /**
+     * 设置标题字体颜色
+     * @param mColor 标题字体颜色
+     */
+    public void setTextColor(int mColor){
+        mTitle.setTextColor(mColor);
     }
 
     /**
      * 设置文本信息
+     * @param center 提示消息内容
      */
-    public void setMessageCenter(CharSequence text){
-        mShowCenter.setText(text);
+    public void setMessageCenter(CharSequence center){
+        mShowCenter.setText(center);
+    }
+
+    /**
+     * 设置提示消息文本大小
+     * @param mSize 消息字体大小
+     */
+    public void setMessageSize(float mSize){
+        mShowCenter.setTextSize(mSize);
+    }
+
+    /**
+     * 设置消息字体颜色
+     * @param mColor 字体颜色
+     */
+    public void setMessageColor(int mColor){
+        mShowCenter.setTextColor(mColor);
     }
 
     /**
      * 设置确定事件
-     * @param listener
+     * @param listener 确定事件
      */
     public void setOnOkListener(View.OnClickListener listener){
         mUPwdOk.setOnClickListener(listener);
@@ -59,13 +174,34 @@ public class MyDialogView extends Dialog {
 
     /**
      * 设置取消事件
-     * @param listener
+     * @param listener 取消事件
      */
     public void setOnCancelListener(View.OnClickListener listener){
         mUPwdCancel.setOnClickListener(listener);
     }
 
+    /**
+     * 设置确定或取消的字体大小
+     * @param mSize
+     */
+    public void setOkOrCancleSize(float mSize){
+        mUPwdOk.setTextSize(mSize);
+        mUPwdCancel.setTextSize(mSize);
+    }
 
+    /**
+     * 设置确定或取消字体的颜色
+     * @param mCorlor
+     */
+    public void setOkOrCancleColor(int mCorlor){
+        mUPwdOk.setTextColor(mCorlor);
+        mUPwdCancel.setTextColor(mCorlor);
+    }
+
+
+    /**
+     * 确定倒计时
+     */
     final Handler handler = new Handler(){
 
         public void handleMessage(Message msg){         // handle message
